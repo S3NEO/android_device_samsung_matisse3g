4f900dca5a483e6c6371c1a3aa47151ccfd67bb4
matteo0026 <marmotta200026@gmail.com>
2021-03-22 22:00:01 +0100

    matisse3g: common_full_phone, various fixes
    common_full_phone = common_full_tablet_wifionly + telephony.mk, so it's the same thing

--- full_matisse3g.mk
+++ full_matisse3g.mk
@@ -15,7 +15,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from s3ve3g device
+# Inherit from matisse3g device
 $(call inherit-product, device/samsung/matisse3g/device.mk)
 
 # Set those variables here to overwrite the inherited values.
--- lineage_matisse3g.mk
+++ lineage_matisse3g.mk
@@ -16,8 +16,7 @@
 #
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/matisse3g/full_matisse3g.mk)
 
